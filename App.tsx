b2daf0efa3241ab5004e84cbf2cbdae1b5d2e9ab
carlos-saiz <useles123456789@gmail.com>
2023-08-31 10:56:50 +0200

    Fetch a api y mostrar nombres

--- App.tsx
+++ App.tsx
@@ -1,12 +1,52 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, SafeAreaView, FlatList, ActivityIndicator } from 'react-native';
+import { useState, useEffect } from 'react';
+
+interface Pokemon {
+  name : string;
+  url: string;
+}
 
 export default function App() {
+  const [pokemon, setPokemon] = useState<Pokemon[]>([]);
+  const [next, setNext] = useState<string>()
+  const [isLoadingMore, setIsLoadingMore]  = useState(false)
+
+  useEffect(() => {
+    fetch('https://pokeapi.co/api/v2/pokemon/')
+      .then((res) => res.json())
+      .then((data) => {
+        setPokemon(data.results);
+        setNext(data.next)
+      });
+  }, []);
+
+  const loadMore = () => {
+    if (isLoadingMore) return;
+    if (next) {
+    setIsLoadingMore(true);
+    fetch(next)
+      .then(res => res.json())
+      .then(data => {
+        setPokemon(prevPokemon => ([...prevPokemon, ...data.results]));
+        setNext(data.next);
+        setIsLoadingMore(false);
+      })
+    
+  }
+  
+}
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <FlatList
+        data={pokemon}
+        keyExtractor={(item) => item.name}
+        renderItem={({ item }) => <Text>{item.name}</Text>}
+        onEndReached={loadMore}
+        ListFooterComponent={() => isLoadingMore ? <ActivityIndicator /> : null}
+      />
+    </SafeAreaView>
   );
 }
 
