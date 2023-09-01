599d20d58ac41de4b09bede08e3fdcefc9fcf9dd
carlos-saiz <useles123456789@gmail.com>
2023-09-01 11:14:07 +0200

    Trae imagenes y nombres de los pokemons

--- App.tsx
+++ App.tsx
@@ -1,60 +1,16 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, SafeAreaView, FlatList, ActivityIndicator } from 'react-native';
-import { useState, useEffect } from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import React from 'react';
+import { ContextProvider } from './src/context/AppContext';
+import AppNavigator from './src/navigation/AppNavigator';
 
-interface Pokemon {
-  name : string;
-  url: string;
-}
 
 export default function App() {
-  const [pokemon, setPokemon] = useState<Pokemon[]>([]);
-  const [next, setNext] = useState<string>()
-  const [isLoadingMore, setIsLoadingMore]  = useState(false)
-
-  useEffect(() => {
-    fetch('https://pokeapi.co/api/v2/pokemon/')
-      .then((res) => res.json())
-      .then((data) => {
-        setPokemon(data.results);
-        setNext(data.next)
-      });
-  }, []);
-
-  const loadMore = () => {
-    if (isLoadingMore) return;
-    if (next) {
-    setIsLoadingMore(true);
-    fetch(next)
-      .then(res => res.json())
-      .then(data => {
-        setPokemon(prevPokemon => ([...prevPokemon, ...data.results]));
-        setNext(data.next);
-        setIsLoadingMore(false);
-      })
-    
-  }
   
-}
-
   return (
-    <SafeAreaView style={styles.container}>
-      <FlatList
-        data={pokemon}
-        keyExtractor={(item) => item.name}
-        renderItem={({ item }) => <Text>{item.name}</Text>}
-        onEndReached={loadMore}
-        ListFooterComponent={() => isLoadingMore ? <ActivityIndicator /> : null}
-      />
-    </SafeAreaView>
+    <ContextProvider>
+    <NavigationContainer>
+        <AppNavigator />
+     </NavigationContainer>
+ </ContextProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ src/screens/HomeScreen.tsx
@@ -0,0 +1,46 @@
+import {
+  StyleSheet,
+  Text,
+  SafeAreaView,
+  FlatList,
+  View,
+} from 'react-native';
+import { useContext } from 'react';
+import { AppContext } from '../context/AppContext';
+import { PokemonCard } from '../components/PokemonCard';
+import { Pokemon } from '../interfaces/Pokemon.interface';
+
+
+export default function HomeScreen() {
+  const { state } = useContext(AppContext); // Obtén el estado de tu contexto global
+
+  // const { pokemons } = state; // Extrae el arry de Pokémon del estado
+  console.log(state.pokemons);
+  // console.log(pokemons[1].url);
+  
+
+
+  return (
+    <SafeAreaView >
+    {state.pokemons.length === 0 ? (
+      <Text>Cargando...</Text>
+    ) : (
+
+      // state.pokemons[0].map((item)=> item.name)
+      <FlatList
+        data={state.pokemons[0]}
+        keyExtractor={(item, index) => index.toString()}
+        renderItem={({ item }) => (
+          
+          <PokemonCard url={item.url} />
+            // <Text>{item.url}</Text>
+          
+        )}
+      />
+    )}
+  </SafeAreaView>
+  );
+}
+
+
+
